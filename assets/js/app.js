d56f2ede6ebd4062aa4b22d3dada1f7eac3109bd
Sean Harger <seanharger93@gmail.com>
2016-02-20 15:14:16 -0800

    Added basic sorting functionality complete with animations.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,6 @@
+var allCards = [];
 var NAMES_DELAY = 100;
-var CARD_TEMPLATE = _.template('<div class="animated lightSpeedIn name-card col s12 m12"><div class="name-card card-panel teal"><span class="white-text"><%- title %></span><span class="right white-text"><%- value %></span></div></div>');
+var CARD_TEMPLATE = _.template('<div class="animated <%= animation %> name-card col s12 m12"><div class="name-card card-panel teal"><span class="white-text"><%- title %></span><span class="right white-text"><%- value %></span></div></div>');
 
 $.fn.extend({
   animateCss: function (animationName, callback) {
@@ -33,9 +34,11 @@ function init() {
     }
 
     importNames(file, function (names) {
-      _.each(names, function (n) {
-        addCard(n.name, n.value); 
-      });
+      allCards = _.concat(allCards, names);
+      addAllCards(allCards, 0, 'lightSpeedIn', function() {});
+      // _.each(names, function (n) {
+        // addCard(n.name, n.value); 
+      // });
     });
   });
 }
@@ -73,10 +76,26 @@ function importNames(file, cb) {
 
 function setGoButtonEnabled(enabled) {
   if (enabled) {
-    $('#btn-go').click(removeAllCards).removeClass('disabled');
+    $('#btn-go').click(function () {
+      setGoButtonEnabled(false);
+      runShuffle(function () { setGoButtonEnabled(true); });
+    }).removeClass('disabled');
   } else {
     $('#btn-go').unbind('click').addClass('disabled');
   }
+}
+
+function shuffleCardsByPriority(cards) {
+  var orderedCards = _.sortBy(cards, 'value').reverse();
+  return orderedCards;
+}
+
+function runShuffle(cb) {
+  removeAllCards(function () { 
+    var shuffled = shuffleCardsByPriority(allCards);
+    
+    addAllCards(shuffled, 500, 'fadeInUp', cb);
+  });
 }
 
 function addCard(title, value) {
@@ -85,19 +104,45 @@ function addCard(title, value) {
   $cardsList.append(CARD_TEMPLATE({title: title, value: value}));
 }
 
-function removeAllCards() {
-  setGoButtonEnabled(false);
+function scrollToBottom() {
+  $(window).scrollTop($(window).height());
+}
+
+function addAllCards(cards, delay, animation, cb) {
+  var $cardsList = $('#cards-list');
+
+  var animations = [];
+  _.each(cards, function(card, index) {
+    var a = new Promise(function (resolve) {
+      setTimeout(function () {
+        var $newElement = $cardsList.append(CARD_TEMPLATE({title: card.name, value: card.value, animation: animation}));
+    
+        $newElement.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() { resolve(); });
+    
+        scrollToBottom();
+      }, delay * index); 
+    });
+
+    animations.push(a);
+  }); 
+
+  Promise.all(animations).then(function() {
+    cb();
+  });
+}
+
+function removeAllCards(cb) {
   var $names = $('.name-card');
 
   var animations = [];
   $names.each(function (index, name) {
     var a = new Promise(function (resolve) {
-      setTimeout(function() {
+      setTimeout(function () {
         $(name).animateCss('bounceOut', function() {
           $(name).css('opacity', '0');
           resolve();
         });
-      }, NAMES_DELAY * index);
+      }, (NAMES_DELAY * Math.random() * 5));
     });
 
     animations.push(a);
@@ -105,7 +150,7 @@ function removeAllCards() {
 
   Promise.all(animations).then(function() {
     $names.remove();
-    setGoButtonEnabled(true);
+    cb();
   });
 }
 
